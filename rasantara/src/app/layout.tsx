d42c1322c607549ba3dc9f961a681eb492307cca
rizkyzulians <rzkyzuliansyah@gmail.com>
2025-10-20 11:34:41 +0700

    added function to register, register success

--- rasantara/src/app/layout.tsx
+++ rasantara/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
+        <Toaster />
         {children}
-        < Footer />
-        
+        <Footer />
       </body>
     </html>
   );
