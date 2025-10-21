574f23f4d94c4882bd2b736cbf1ce29cffa393e7
rizkyzulians <rzkyzuliansyah@gmail.com>
2025-10-21 12:41:14 +0700

    change the google button color

--- rasantara/src/app/login/page.tsx
+++ rasantara/src/app/login/page.tsx
@@ -107,7 +107,7 @@ export default function Login() {
           <button
             type="button"
             onClick={() => signIn("google", { callbackUrl: "/" })}
-            className="w-full bg-white border border-gray-300 text-gray-700 py-2 px-4 rounded-md hover:bg-[#E0A106] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 flex items-center justify-center gap-3 transition-colors"
+            className="w-full bg-[#E0A106] border border-gray-300 text-white py-2 px-4 rounded-md hover:bg-[#D35400] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 flex items-center justify-center gap-3 transition-colors"
           >
             <svg className="w-5 h-5" viewBox="0 0 24 24">
               <path
